862ad7e9d73cb17ee1d013f9cc2eb7cc955db319
edward795 <ayyappadasgb777999@gmail.com>
2024-06-17 21:06:03 +0530

    Design Patterns : singleton-1

--- src/main/java/singleton/Main.java
+++ src/main/java/singleton/Main.java
@@ -1,17 +1,31 @@
 package singleton;
 
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public Main() throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+            LazySingleton lazySingleton = LazySingleton.getInstance();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("object.obj"));
+            objectOutputStream.writeObject(lazySingleton);
+            objectOutputStream.close();
+            ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("object.obj"));
+            LazySingleton deserializedLazy = (LazySingleton) objectInputStream.readObject();
+            objectInputStream.close();
+            System.out.println("Object 1 :" + lazySingleton.hashCode());
+            System.out.println("Object 2 :" + deserializedLazy.hashCode());
+
+            SerializableSingleton serializableSingleton=SerializableSingleton.getInstance();
+            ObjectOutputStream objectOutputStream1=new ObjectOutputStream(new FileOutputStream("object1.obj"));
+            objectOutputStream1.writeObject(serializableSingleton);
+            objectOutputStream1.close();
+            ObjectInputStream objectInputStream1=new ObjectInputStream(new FileInputStream("object1.obj"));
+            SerializableSingleton deserializedInstance=(SerializableSingleton) objectInputStream1.readObject();
+            objectInputStream1.close();
+            System.out.println("Object 1 : "+serializableSingleton.hashCode());
+            System.out.println("Object 2 : "+deserializedInstance.hashCode());
+
     }
-    LazySingleton lazySingleton = LazySingleton.getInstance();
-    FileOutputStream fos = new FileOutputStream("EmployeeObject.ser");
-    ObjectOutputStream oos = new ObjectOutputStream(fos);
-    // write object to file
-    oos.writeObject(lazySingleton);
 }
